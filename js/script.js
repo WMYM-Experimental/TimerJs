d6d9c7f870edb3a0f2086b4d21deb8aee70535ef
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-08-23 10:28:26 -0500

    made the style in the css file and declare variables in the script file

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+const mili = document.getElementById("mili");
+
+const startBtn = document.getElementById("button-start");
+const stopBtn = document.getElementById("button-stop");
+const resettBtn = document.getElementById("button-reset");
+
+let seconds = 00;
+let miliseconds = 00;
+let minutes = 00;
+
+let interval;
